cc82395972da46b3d5635ae417cc523328a15bd3
Aespino <albaliciae@gmail.com>
2022-06-29 14:58:28 -0500

    linked assest to front end/bug fix

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-console.log(path.join(__dirname, "Public"));
-//app.use(express.static(path.join(__dirname, "Public")));
+console.log(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
