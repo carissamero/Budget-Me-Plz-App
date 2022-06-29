6e5c6bab128cade02fb6ced4872d64e98be895f9
Aespino <albaliciae@gmail.com>
2022-06-28 22:21:56 -0500

    fix attempts

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'super secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
+  secret: "super secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -30,10 +30,11 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+console.log(path.join(__dirname, "Public"));
+//app.use(express.static(path.join(__dirname, "Public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening" + PORT));
+  app.listen(PORT, () => console.log("Now listening" + PORT));
 });
