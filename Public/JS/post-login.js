7d964cbf2834b3b4f918c76edc9af38264c7f9b1
Aespino <albaliciae@gmail.com>
2022-06-29 15:23:27 -0500

    fixed a few bugs with log in

--- Public/JS/post-login.js
+++ Public/JS/post-login.js
@@ -1,38 +1,40 @@
 //Change routes, have success/fail message append to the page rather than the alert? //
 
-const loginFormHandler = (event) => {
-    event.preventDefault();
-    console.log("works");
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("works");
 
-    const userEmail = document.querySelector("#email").value.trim();
-    const userPassword = document.querySelector("#password").value.trim();
-    let loginObject = {
-        email: userEmail,
-        password: userPassword,
-    };
-    if (userEmail && userPassword) {
-        const response = await fetch("api/post/login", {
-            method: "POST",
-            body: JSON.stringify(loginObject),
-            headers: { "Content-Type": "application/json" },
-        });
+  const userEmail = document.querySelector("#email").value.trim();
+  const userPassword = document.querySelector("#password").value.trim();
+  let loginObject = {
+    email: userEmail,
+    password: userPassword,
+  };
+  if (userEmail && userPassword) {
+    const response = await fetch("api/post/login", {
+      method: "POST",
+      body: JSON.stringify(loginObject),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace("/");
-        } else {
-            alert("Failed to log in");
-        }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
     }
+  }
 };
 
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.location.replace('/signup/');
-};
+  document.location.replace("/signup/");
+}
 
 document
-    .querySelector("#login-button")
-    .addEventListener("click", loginFormHandler);
+  .querySelector("#login-button")
+  .addEventListener("click", loginFormHandler);
 
-document.querySelector('#signup-btn').addEventListener('submit', signupFormHandler);
+document
+  .querySelector("#signup-btn")
+  .addEventListener("submit", signupFormHandler);
