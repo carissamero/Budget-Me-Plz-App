20e9ebbf87d73f94554361262cd646e097b3a552
enajera1 <eriknajera013@gmail.com>
2022-07-03 20:12:11 -0500

    sign-up complete

--- Public/JS/post-new-user.js
+++ Public/JS/post-new-user.js
@@ -1,11 +1,10 @@
 //Change routes, have success/fail message append to the page rather than the alert? //
-console.log(`I'm here`);
 
 async function signupFormHandler(event)  {
     event.preventDefault();
     
-    const firstName = document.querySelector("#first-name").value.trim();
-    const lastName = document.querySelector("#last-name").value.trim();
+    const first_name = document.querySelector("#first-name").value.trim();
+    const last_name = document.querySelector("#last-name").value.trim();
     const email = document.querySelector("#email-signup").value.trim();
     const password = document.querySelector("#password-signup").value.trim();
     const pay_day = document.querySelector('#pay_day-signup').value.trim();
@@ -15,9 +14,7 @@ async function signupFormHandler(event)  {
     const credit_card = document.querySelector('#credit_card-signup').value.trim();
     const new_checking = document.querySelector('#new_checking-signup').value.trim();
 
-    console.log('clicked!!!');
-
-    if (firstName && lastName && email && password && pay_day && monthly_income && checking && savings && credit_card && new_checking) {
+    if (first_name && last_name && email && password && pay_day && monthly_income && checking && savings && credit_card && new_checking) {
         // let user = {
         //     first_name: firstName,
         //     last_name: lastName,
@@ -32,12 +29,12 @@ async function signupFormHandler(event)  {
         //     new_checking: 0,
         // };
 
-        const response = await fetch("/user", {
+        const response = await fetch("/user/", {
             method: 'post',
             // body: JSON.stringify(user),
             body: JSON.stringify({
-                firstName,
-                lastName,
+                first_name,
+                last_name,
                 email,
                 password,
                 pay_day,
@@ -47,14 +44,14 @@ async function signupFormHandler(event)  {
                 credit_card,
                 new_checking
             }),
-            headers: { "Content-Type": "application/json" },
+            headers: { "Content-Type": "application/json" }
         });
 
         if (response.ok) {
             document.location.replace("/user");
         } else {
             alert(
-                "User already exists. Please log in or sign up with a different email."
+                response.statusText
             );
         }
     }
