66b67783ed317189c94cbc74f1da934f9ee5e746
Aespino <albaliciae@gmail.com>
2022-07-03 22:27:52 -0500

    deploy heroku/connect to jawsdb

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use(routes);
 app.use(require('./Controllers/'));
 
+const PORT = process.env.PORT || 3001;
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening" + PORT));
 });
