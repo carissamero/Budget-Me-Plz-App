788c1fe619b06d18df91ad9ba81c8b1053862060
Aespino <albaliciae@gmail.com>
2022-07-04 17:07:29 -0500

    updated lockfile

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require("./Config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || 3001);
 
 const hbs = exphbs.create({ helpers });
 
